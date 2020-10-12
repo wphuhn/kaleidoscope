1c414b7beda06b67846de79120bcc04b5636672a
William Huhn <william.paul.huhn@gmail.com>
2020-10-12 16:09:57 -0400

    Add assignment operator for stack variables
    Note that this commit does not allow users to create their own stack
    variables, merely redefine pre-existing ones, e.g. loop iterators and
    function parameters.

--- src/toy.cpp
+++ src/toy.cpp
@@ -151,6 +151,7 @@ public:
   VariableExprAST(const std::string &Name) : Name(Name) {}
 
   Value *codegen() override;
+  const std::string &getName() const { return Name; }
 };
 
 /// UnaryExprAST - Expression class for a unary operator.
@@ -683,6 +684,27 @@ Value *UnaryExprAST::codegen() {
 }
 
 Value *BinaryExprAST::codegen() {
+  // Special case '=' because we don't want to emit the LHS as an expression
+  if (Op == '=') {
+    // Assignment requires the LHS to be an identifier
+    VariableExprAST *LHSE = dynamic_cast<VariableExprAST*>(LHS.get());
+    if (!LHSE)
+      return LogErrorV("destination of '=' must be a variable");
+
+    // Codegen the RHS
+    Value *Val = RHS->codegen();
+    if (!Val)
+      return nullptr;
+
+    // Look up the name
+    Value *Variable = NamedValues[LHSE->getName()];
+    if (!Variable)
+      return LogErrorV("Unknown variable name");
+
+    Builder.CreateStore(Val, Variable);
+    return Val;
+  }
+
   Value *L = LHS->codegen();
   Value *R = RHS->codegen();
   if (!L || !R)
@@ -1090,6 +1112,7 @@ int main() {
 
   // Install standard binary operators.
   // 1 is lowest precedence.
+  BinopPrecedence['='] = 2;
   BinopPrecedence['<'] = 10;
   BinopPrecedence['+'] = 20;
   BinopPrecedence['-'] = 20;
